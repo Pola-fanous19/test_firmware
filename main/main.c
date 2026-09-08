1e598c9a60b826384ecfefc46abfe307dfa2d4d8
Pola Magdy Farag Fanous <pola.fanous19@gmail.com>
2026-09-08 16:08:25 +0300

    Update LED control to use BLINK_GPIO

--- main/main.c
+++ main/main.c
@@ -6,7 +6,13 @@
 
 void app_main(void)
 {
-    gpio_reset_pin(LED_GPIO);
-    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
-    gpio_set_level(LED_GPIO, 1);
+    gpio_reset_pin(BLINK_GPIO);
+    gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
+
+    while (true) {
+        gpio_set_level(BLINK_GPIO, 1);
+        vTaskDelay(pdMS_TO_TICKS(BLINK_PERIOD_MS));
+        gpio_set_level(BLINK_GPIO, 0);
+        vTaskDelay(pdMS_TO_TICKS(BLINK_PERIOD_MS));
+    }
 }
